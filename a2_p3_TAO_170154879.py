c85030195e52255b20cfcc334bd8f96c82a17a51
Kevin <kt4cty@gmail.com>
2024-04-05 21:23:39 -0400

    bug fixes and cuda amp

--- a2_p3_TAO_170154879.py
+++ a2_p3_TAO_170154879.py
@@ -12,7 +12,7 @@ import matplotlib.pyplot as plt
 class Classifier(torch.nn.Module):
     def __init__(self, basemodel):
         super().__init__()
-        self.model = basemodel.clone()
+        self.model = basemodel
         self.linear = torch.nn.Linear(basemodel.config.hidden_size, 1)
 
     def forward(self, input_ids, attention_mask):
@@ -24,7 +24,7 @@ class Classifier(torch.nn.Module):
 class Regression(torch.nn.Module):
     def __init__(self, basemodel):
         super().__init__()
-        self.model = basemodel.clone()
+        self.model = basemodel
         self.linear = torch.nn.Linear(basemodel.config.hidden_size, 1)
 
     def forward(self, input_ids, attention_mask):
@@ -56,41 +56,45 @@ class NoResidualRobertaModel(RobertaModel):
         self.encoder = NoResidualRobertaEncoder(config)
         
 
-def fineTune_boolQ(model, name = 'model'):
+def fineTune_boolQ(model, name = 'model', debug=True):
     train_dataset = boolq_dataset['train'].map(lambda example: {'label': 1 if example['answer'] == True else 0})
-    loss_function = torch.nn.BCELoss()
+    loss_function = torch.nn.BCEWithLogitsLoss()
     optimizer = torch.optim.AdamW(model.parameters(), lr=2e-5)
+    scaler = torch.cuda.amp.GradScaler()
 
     train_losses = []
-    print(f'Fine-tuning {name} on BoolQ')
+    if debug: print(f'Fine-tuning {name} on BoolQ')
     for epoch in range(5):
         model.train()
-        print(f"\nEpoch {epoch}", end=' ')
+        # print(f"Epoch {epoch}")
         epoch_loss = 0
         for i, batch in enumerate(DataLoader(train_dataset, batch_size=8, shuffle=True, pin_memory=True, num_workers=4)):
-            # if i % 200 == 0: print(f"\nBatch {i} - {i+200} ...", end=' ')
+            # if i % 200 == 0: print(f"Batch {i} - {i+200} ...")
             optimizer.zero_grad()
             prompts = [f"{passage}\n{question}?\n" for passage, question in zip(batch['passage'], batch['question'])]
             inputs = tokenizer(prompts, padding=True, truncation=True, max_length=512, return_tensors='pt').to(device)
             labels = batch['label'].float().to(device)#.to(dtype=torch.float16)
-            logits = model(**inputs).squeeze()#.to(dtype=torch.float16)
-
-            loss = loss_function(logits, labels)
-            loss.backward()
-            optimizer.step()
+            with torch.cuda.amp.autocast():
+                logits = model(**inputs).squeeze()#.to(dtype=torch.float16)
+                loss = loss_function(logits, labels)
+            scaler.scale(loss).backward()
+            #loss.backward()
+            scaler.step(optimizer)
+            #optimizer.step()
+            scaler.update()
             epoch_loss += loss.item()
         train_losses.append(epoch_loss / len(train_dataset))
-        print(f"\nEpoch {epoch} loss: {epoch_loss / len(train_dataset)}")
+        if debug: print(f"Epoch {epoch} loss: {epoch_loss / len(train_dataset)}")
     return train_losses
 
-def evaluate_boolQ(model, name = 'model'):
+def evaluate_boolQ(model, name = 'model', debug=True):
     validate_dataset = boolq_dataset['validation'].map(lambda example: {'label': 1 if example['answer'] == True else 0})
     model.eval()
     predictions = []
     true_labels = []
-    print(f'Evaluating {name} on BoolQ', end=' ')
+    if debug: print(f'Evaluating {name} on BoolQ', end=' ')
     with torch.no_grad():
-        for i, batch in enumerate(DataLoader(validate_dataset, batch_size=16, pin_memory=True, num_workers=4)):
+        for i, batch in enumerate(DataLoader(validate_dataset, batch_size=8, pin_memory=True, num_workers=4)):
             # if i % 200 == 0: print(f"\nBatch {i} - {i+200} ...", end=' ')
             prompts = [f"{passage}\n{question}?\n" for passage, question in zip(batch['passage'], batch['question'])]
             inputs = tokenizer(prompts, padding=True, truncation=True, max_length=512, return_tensors='pt').to(device)
@@ -99,43 +103,47 @@ def evaluate_boolQ(model, name = 'model'):
 
             predictions.extend(logits.cpu().numpy() > 0.5)
             true_labels.extend(labels.cpu().numpy())
-        print()
+        if debug: print()
 
         accuracy = accuracy_score(true_labels, predictions)
         f1 = f1_score(true_labels, predictions)
-        precision_yes = precision_score(true_labels, predictions, pos_label=1)
-        recall_yes = recall_score(true_labels, predictions, pos_label=1)
-        precision_no = precision_score(true_labels, predictions, pos_label=0)
-        recall_no = recall_score(true_labels, predictions, pos_label=0)
+        precision_yes = precision_score(true_labels, predictions, pos_label=1, zero_division=0)
+        recall_yes = recall_score(true_labels, predictions, pos_label=1, zero_division=0)
+        precision_no = precision_score(true_labels, predictions, pos_label=0, zero_division=0)
+        recall_no = recall_score(true_labels, predictions, pos_label=0, zero_division=0)
     return accuracy, f1, precision_yes, recall_yes, precision_no, recall_no
 
-def fineTune_emoBank(model, name = 'model'):
+def fineTune_emoBank(model, name = 'model', debug=True):
     train_dataset = emo_dataset['train']
     loss_function = torch.nn.MSELoss()
     optimizer = torch.optim.AdamW(model.parameters(), lr=2e-5)
+    scaler = torch.cuda.amp.GradScaler()
 
     train_losses = []
-    print(f'Fine-tuning {name} on EmoBank')
+    if debug: print(f'Fine-tuning {name} on EmoBank')
     for epoch in range(5):
         model.train()
-        print(f"\nEpoch {epoch}", end=' ')
+        # print(f"Epoch {epoch}")
         epoch_loss = 0
         for i, batch in enumerate(DataLoader(train_dataset, batch_size=8, shuffle=True, pin_memory=True, num_workers=4)):
-            # if i % 200 == 0: print(f"\nBatch {i} - {i+200} ...", end=' ')
+            # if i % 200 == 0: print(f"Batch {i} - {i+200} ...")
             optimizer.zero_grad()
             inputs = tokenizer(batch['text'], padding=True, truncation=True, max_length=512, return_tensors='pt').to(device)
             labels = batch['label'].float().to(device)
-            logits = model(**inputs).squeeze()
-
-            loss = loss_function(logits, labels)
-            loss.backward()
-            optimizer.step()
+            with torch.cuda.amp.autocast():
+                logits = model(**inputs).squeeze()
+                loss = loss_function(logits, labels)
+            scaler.scale(loss).backward()
+            #loss.backward()
+            scaler.step(optimizer)
+            #optimizer.step()
+            scaler.update()
             epoch_loss += loss.item()
         train_losses.append(epoch_loss / len(train_dataset))
-        print(f"\nEpoch {epoch} loss: {epoch_loss / len(train_dataset)}")
+        if debug: print(f"Epoch {epoch} loss: {epoch_loss / len(train_dataset)}")
     return train_losses
 
-def evaluate_emoBank(model, name = 'model'):
+def evaluate_emoBank(model, name = 'model', debug=True):
     validate_dataset = emo_dataset['validation']
     test_dataset = emo_dataset['test']
     model.eval()
@@ -143,7 +151,7 @@ def evaluate_emoBank(model, name = 'model'):
     validate_labels = []
     test_preds = []
     test_labels = []
-    print(f'Evaluating {name} on EmoBank', end=' ')
+    if debug: print(f'Evaluating {name} on EmoBank', end=' ')
     with torch.no_grad():
         for i, batch in enumerate(DataLoader(validate_dataset, batch_size=8, pin_memory=True, num_workers=4)):
             # if i % 200 == 0: print(f"\nBatch {i} - {i+200} ...", end=' ')
@@ -175,11 +183,11 @@ if __name__ == '__main__':
 
     boolq_dataset = load_dataset('google/boolq')
     emo_dataset = load_dataset('Blablablab/SOCKET', 'emobank#valence', trust_remote_code=True)
-    model = RobertaModel.from_pretrained('distilroberta-base').to(device)
     tokenizer = RobertaTokenizerFast.from_pretrained('distilroberta-base', pad_token='<pad>', unk_token='<unk>')
 
     if True: # Classification
-        classifier = Classifier(model).to(device)#.to(dtype=torch.float16)
+        classifier = RobertaModel.from_pretrained('distilroberta-base').to(device)
+        classifier = Classifier(classifier).to(device)#.to(dtype=torch.float16)
         train_losses = fineTune_boolQ(classifier)
 
         plt.plot(train_losses)
@@ -196,10 +204,8 @@ if __name__ == '__main__':
         print(f"     No: prec: {precision_no:.3f}, rec: {recall_no:.3f}, f1: {2*(precision_no*recall_no)/(precision_no+recall_no):.3f}")
 
     if True: # Regression
-        validate_dataset = emo_dataset['validation']
-        test_dataset = emo_dataset['test']
-
-        regression = Regression(model).to(device)#.to(dtype=torch.float16)
+        regression = RobertaModel.from_pretrained('distilroberta-base').to(device)
+        regression = Regression(regression).to(device)#.to(dtype=torch.float16)
         train_losses = fineTune_emoBank(regression)
 
         plt.plot(train_losses)
@@ -249,17 +255,21 @@ if __name__ == '__main__':
         distilRB_nores1 = Classifier(distilRB_nores1).to(device)
         distilRB_nores2 = Regression(distilRB_nores2).to(device)
 
-        train_losses1 = fineTune_boolQ(distilRB_rand1, 'distilRB_rand')
+        # train_losses1 = fineTune_boolQ(distilRB_rand1, 'distilRB_rand')
         train_losses2 = fineTune_boolQ(distilRB_KQ1, 'distilRB_KQ')
         train_losses3 = fineTune_boolQ(distilRB_nores1, 'distilRB_nores')
-        train_losses4 = fineTune_emoBank(distilRB_rand2, 'distilRB_rand')
+        # train_losses4 = fineTune_emoBank(distilRB_rand2, 'distilRB_rand')
         train_losses5 = fineTune_emoBank(distilRB_KQ2, 'distilRB_KQ')
         train_losses6 = fineTune_emoBank(distilRB_nores2, 'distilRB_nores')
 
-        fig, axs = plt.subplots(2, 3, figsize=(16, 9))
+        # fig, axs = plt.subplots(2, 3, figsize=(16, 9))
+        fig, axs = plt.subplots(2, 2, figsize=(16, 9))
         axs = axs.flatten()
-        for i, (ax, train_losses, title) in enumerate(zip(axs, [train_losses1, train_losses2, train_losses3, train_losses4, train_losses5, train_losses6], 
-                                                   ['distilRB_rand BoolQ', 'distilRB_KQ BoolQ', 'distilRB_nores BoolQ', 'distilRB_rand EmoBank', 'distilRB_KQ EmoBank', 'distilRB_nores EmoBank'])):
+        # for i, (ax, train_losses, title) in enumerate(zip(axs, [train_losses1, train_losses2, train_losses3, train_losses4, train_losses5, train_losses6], 
+        #                                            ['distilRB_rand BoolQ', 'distilRB_KQ BoolQ', 'distilRB_nores BoolQ', 'distilRB_rand EmoBank', 'distilRB_KQ EmoBank', 'distilRB_nores EmoBank'])):
+        
+        for i, (ax, train_losses, title) in enumerate(zip(axs, [train_losses2, train_losses3, train_losses5, train_losses6], 
+                                                   ['distilRB_KQ BoolQ', 'distilRB_nores BoolQ', 'distilRB_KQ EmoBank', 'distilRB_nores EmoBank'])):
             ax.plot(train_losses)
             ax.set_xlabel('Epoch')
             ax.set_ylabel('Loss')
@@ -268,20 +278,20 @@ if __name__ == '__main__':
 
         print('\nCheckpoint 3.4')
         print('boolq validation set: ')
-        acc, f1, _, _, _, _ = evaluate_boolQ(classifier, 'distilroberta')
+        acc, f1, _, _, _, _ = evaluate_boolQ(classifier, 'distilroberta', debug=False)
         print(f'distilroberta: overall acc: {acc:.3f}, f1: {f1:.3f}')
-        acc, f1, _, _, _, _ = evaluate_boolQ(distilRB_rand1, 'distilRB_rand')
+        acc, f1, _, _, _, _ = evaluate_boolQ(distilRB_rand1, 'distilRB_rand', debug=False)
         print(f'distilRB-rand: overall acc: {acc:.3f}, f1: {f1:.3f}')
-        acc, f1, _, _, _, _ = evaluate_boolQ(distilRB_KQ1, 'distilRB_KQ')
+        acc, f1, _, _, _, _ = evaluate_boolQ(distilRB_KQ1, 'distilRB_KQ', debug=False)
         print(f'distilRB-KQ: overall acc: {acc:.3f}, f1: {f1:.3f}')
-        acc, f1, _, _, _, _ = evaluate_boolQ(distilRB_nores1, 'distilRB_nores')
+        acc, f1, _, _, _, _ = evaluate_boolQ(distilRB_nores1, 'distilRB_nores', debug=False)
         print(f'distilRB-nores: overall acc: {acc:.3f}, f1: {f1:.3f}')
         print('emobank validation set: ')
-        mae, r, _, _ = evaluate_emoBank(regression, 'distilroberta')
+        mae, r, _, _ = evaluate_emoBank(regression, 'distilroberta', debug=False)
         print(f'distilroberta: mae: {mae:.3f}, r: {r:.3f}')
-        mae, r, _, _ = evaluate_emoBank(distilRB_rand2, 'distilRB_rand')
+        mae, r, _, _ = evaluate_emoBank(distilRB_rand2, 'distilRB_rand', debug=False)
         print(f'distilRB-rand: mae: {mae:.3f}, r: {r:.3f}')
-        mae, r, _, _ = evaluate_emoBank(distilRB_KQ2, 'distilRB_KQ')
+        mae, r, _, _ = evaluate_emoBank(distilRB_KQ2, 'distilRB_KQ', debug=False)
         print(f'distilRB-KQ: mae: {mae:.3f}, r: {r:.3f}')
-        mae, r, _, _ = evaluate_emoBank(distilRB_nores2, 'distilRB_nores')
+        mae, r, _, _ = evaluate_emoBank(distilRB_nores2, 'distilRB_nores', debug=False)
         print(f'distilRB-nores: mae: {mae:.3f}, r: {r:.3f}')
